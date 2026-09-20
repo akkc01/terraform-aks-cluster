03c8d2e06857463fb98ff498d0f1b30fe11ccdd0
Amit K Verma <amitkkc01@gmail.com>
2026-09-20 16:55:49 +0530

    Update backend.tf

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,12 +1,8 @@
 terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.100"
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "akkctfstate"
+    container_name       = "tfstate"
+    key                  = "aks.tfstate"
   }
-
-  backend "azurerm" {}
 }
