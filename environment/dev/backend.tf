5a4c50213df74434a7c61264c281bd9a98744639
Amit K Verma <amitkkc01@gmail.com>
2026-09-20 17:47:54 +0530

    Update backend.tf

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "akkctfstate"
-    container_name       = "tfstate"
-    key                  = "aks.tfstate"
+    resource_group_name  = "akkc"
+    storage_account_name = "axionteststgakk01"
+    container_name       = "axion-statefile"
+    key                  = "aks.dev.tfstate"
   }
 }
